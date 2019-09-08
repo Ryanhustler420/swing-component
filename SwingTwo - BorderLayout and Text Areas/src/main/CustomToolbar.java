16d49f86b0debdc10efa1b725b1ba47cb707eaf7
Gaurav Gupta <gouravgupta840@gmail.com>
2019-09-08 13:12:09 +0530

    Creating A Bad Way Of Communication Between Components

--- SwingTwo - BorderLayout and Text Areas/src/main/CustomToolbar.java
+++ SwingTwo - BorderLayout and Text Areas/src/main/CustomToolbar.java
@@ -1,26 +1,49 @@
 package main;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class CustomToolbar extends JPanel {
+public class CustomToolbar extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 
 	private JButton helloBtn;
 	private JButton goodbayBtn;
 
+	private TextPanel textPanel;
+
 	public CustomToolbar() {
 
 		helloBtn = new JButton("Hello");
 		goodbayBtn = new JButton("Goodbay");
+
+		helloBtn.addActionListener(this);
+		goodbayBtn.addActionListener(this);
 
 		setLayout(new FlowLayout(FlowLayout.LEFT));
 
 		add(helloBtn);
 		add(goodbayBtn);
 
+	}
+
+	public void setTextPanel(TextPanel textPanel) {
+		this.textPanel = textPanel;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton clicked = (JButton) e.getSource();
+		if (clicked == helloBtn) {
+			// System.out.println("Hello");
+			textPanel.appendText("Hello \n");
+		} else {
+			// System.out.println("Goodbay");
+			textPanel.appendText("Goodbye \n");
+		}
 	}
 }
--- SwingTwo - BorderLayout and Text Areas/src/main/MainFrame.java
+++ SwingTwo - BorderLayout and Text Areas/src/main/MainFrame.java
@@ -1,10 +1,6 @@
 package main;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame {
@@ -12,7 +8,6 @@ public class MainFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	private TextPanel textPanel;
-	private JButton btn;
 	private CustomToolbar toolbar;
 
 	public MainFrame() {
@@ -22,17 +17,11 @@ public class MainFrame extends JFrame {
 
 		textPanel = new TextPanel();
 		toolbar = new CustomToolbar();
-		btn = new JButton("Click Me!");
 
-		btn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				textPanel.appendText("Welcome to facebook \n");
-			}
-		});
-		
+		toolbar.setTextPanel(textPanel);
+
 		add(toolbar, BorderLayout.NORTH); // top
 		add(textPanel, BorderLayout.CENTER);
-		add(btn, BorderLayout.SOUTH); // bottom
 
 		setSize(600, 500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
